c5f9afa3fdda781f4fedf4aecf1bf7732a92e5ae
Mahatana Rakotomalala <hei.mahatana@gmail.com>
2026-08-11 11:59:27 +0300

    add errorHandler

---
+++ src/middlewares/asyncHandler.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction, RequestHandler } from "
+
+export const asyncHandler =
+  (fn: RequestHandler): RequestHandler =>
+  (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
