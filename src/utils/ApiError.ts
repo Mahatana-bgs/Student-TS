d52cd4c2fb1ce65eb75d9f15dd62af6d22b84847
Mahatana Rakotomalala <hei.mahatana@gmail.com>
2026-08-19 02:16:43 +0300

    add mmethod  to format error

--- src/utils/ApiError.ts
+++ src/utils/ApiError.ts
@@ -1,10 +1,18 @@
-//apierror.ts
 export class ApiError extends Error {
   statusCode: number;
 
   constructor(statusCode: number, message: string) {
     super(message);
     this.statusCode = statusCode;
+    this.name = 'ApiError';
     Object.setPrototypeOf(this, ApiError.prototype);
+  }
+
+  toJSON() {
+    return {
+      success: false,
+      message: this.message,
+      statusCode: this.statusCode,
+    };
   }
 }
